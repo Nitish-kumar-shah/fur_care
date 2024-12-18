14c69040daf5fc78994e247e51ca5c14d301ff15
nitish <nitish.zyn@gmail.com>
2024-12-18 19:43:39 +0530

    Make the cart-quantity interactive

--- SCRIPTS/product.js
+++ SCRIPTS/product.js
@@ -143,6 +143,11 @@ document.querySelectorAll(".js-add-to-cart-btn").forEach((button) => {
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
