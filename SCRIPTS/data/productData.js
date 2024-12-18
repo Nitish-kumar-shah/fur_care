3b9d22fad6880e0a78c4209cd6fd54d40edcbcb9
nitish <nitish.zyn@gmail.com>
2024-12-18 15:13:19 +0530

    create seprate productData.js for product item

---
+++ SCRIPTS/data/productData.js
@@ -0,0 +1,148 @@
+const products = [
+  {
+    image: "/IMAGES/dp-cat2.webp",
+    name: "This is Good product",
+    rating: {
+      stars: 4.5,
+      conut: 87,
+    },
+    priceCents: 1098,
+  },
+  {
+    image: "/IMAGES/dog-bg.jpg",
+    name: " hola amigo jfdjkfd djfdjfdf jdkjfdf dkjfdfjd jksdjfdf kdjfd",
+    rating: {
+      stars: 3,
+      conut: 98,
+    },
+    priceCents: 1087,
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+  {
+    image: "",
+    name: "",
+    rating: {
+      stars: 4,
+      count: 494,
+    },
+  },
+];
--- SCRIPTS/product.js
+++ SCRIPTS/product.js
@@ -75,27 +75,6 @@ headerImg.addEventListener("mouseleave", () => {
   autoScroll = setInterval(scrollRight, 3000);
 });
 
-const products = [
-  {
-    image: "/IMAGES/dp-cat2.webp",
-    name: "This is Good product",
-    rating: {
-      stars: 4.5,
-      conut: 87,
-    },
-    priceCents: 1098,
-  },
-  {
-    image: "/IMAGES/dog-bg.jpg",
-    name: " hola amigo jfdjkfd djfdjfdf jdkjfdf dkjfdfjd jksdjfdf kdjfd",
-    rating: {
-      stars: 3,
-      conut: 98,
-    },
-    priceCents: 1087,
-  },
-];
-
 let productsHTML = "";
 
 products.forEach((product) => {
